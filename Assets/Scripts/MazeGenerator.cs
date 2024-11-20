c01f56fb05eea15a72048cb2883c10d5ff66608e
mohammedbentahar <mohammed.bentahar.1@ens.etsmtl.ca>
2024-11-19 22:02:18 -0500

    Génération de la map de façon procédurale

---
+++ Assets/Scripts/MazeGenerator.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Définition des états des murs dans le labyrinthe
+/// </summary>
+[Flags]
+public enum WallState
+{
+    // Aucun mur
+    NONE = 0, // 0000
+
+    // Murs individuels
+    LEFT = 1,   // 0001
+    RIGHT = 2,  // 0010
+    UP = 4,     // 0100
+    DOWN = 8,   // 1000
+
+    // Indique qu'une cellule a été visitée
+    VISITED = 128 // 1000 0000
+}
+
+/// <summary>
+/// Structure pour représenter une position dans la grille
+/// </summary>
+public struct Position
+{
+    public int X;
+    public int Y;
+}
+
+/// <summary>
+/// Structure pour représenter un voisin avec un mur partagé
+/// </summary>
+public struct Neighbour
+{
+    public Position Position;
+    public WallState SharedWall;
+}
+
+/// <summary>
+/// Générateur de labyrinthes utilisant l'algorithme de backtracking récursif
+/// </summary>
+public static class MazeGenerator
+{
+    /// <summary>
+    /// Retourne le mur opposé pour un mur donné
+    /// </summary>
+    /// <param name="wall">Mur dont on veut l'opposé</param>
+    /// <returns>Mur opposé</returns>
+    private static WallState GetOppositeWall(WallState wall)
+    {
+        return wall switch
+        {
+            WallState.RIGHT => WallState.LEFT,
+            WallState.LEFT => WallState.RIGHT,
+            WallState.UP => WallState.DOWN,
+            WallState.DOWN => WallState.UP,
+            _ => WallState.NONE
+        };
+    }
+
+    /// <summary>
+    /// Applique l'algorithme de backtracking récursif pour générer un labyrinthe
+    /// </summary>
+    /// <param name="maze">Matrice représentant le labyrinthe</param>
+    /// <param name="width">Largeur de la grille</param>
+    /// <param name="height">Hauteur de la grille</param>
+    /// <returns>Labyrinthe généré</returns>
+    private static WallState[,] ApplyRecursiveBacktracker(WallState[,] maze, int width, int height)
+    {
+        var rng = new System.Random(); // Générateur aléatoire
+        var positionStack = new Stack<Position>();
+        var initialPosition = new Position
+        {
+            X = rng.Next(0, width),
+            Y = rng.Next(0, height)
+        };
+
+        maze[initialPosition.X, initialPosition.Y] |= WallState.VISITED;
+        positionStack.Push(initialPosition);
+
+        while (positionStack.Count > 0)
+        {
+            var current = positionStack.Pop();
+            var neighbours = GetUnvisitedNeighbours(current, maze, width, height);
+
+            if (neighbours.Count > 0)
+            {
+                positionStack.Push(current);
+
+                // Sélection d'un voisin aléatoire
+                var randomNeighbour = neighbours[rng.Next(neighbours.Count)];
+
+                // Mise à jour des murs entre les cellules
+                maze[current.X, current.Y] &= ~randomNeighbour.SharedWall;
+                maze[randomNeighbour.Position.X, randomNeighbour.Position.Y] &= ~GetOppositeWall(randomNeighbour.SharedWall);
+                maze[randomNeighbour.Position.X, randomNeighbour.Position.Y] |= WallState.VISITED;
+
+                positionStack.Push(randomNeighbour.Position);
+            }
+        }
+
+        return maze;
+    }
+
+    /// <summary>
+    /// Récupère les voisins non visités d'une position donnée
+    /// </summary>
+    /// <param name="p">Position actuelle</param>
+    /// <param name="maze">Matrice du labyrinthe</param>
+    /// <param name="width">Largeur de la grille</param>
+    /// <param name="height">Hauteur de la grille</param>
+    /// <returns>Liste des voisins non visités</returns>
+    private static List<Neighbour> GetUnvisitedNeighbours(Position p, WallState[,] maze, int width, int height)
+    {
+        var neighbours = new List<Neighbour>();
+
+        // Vérifie le voisin à gauche
+        if (p.X > 0 && !maze[p.X - 1, p.Y].HasFlag(WallState.VISITED))
+        {
+            neighbours.Add(new Neighbour
+            {
+                Position = new Position { X = p.X - 1, Y = p.Y },
+                SharedWall = WallState.LEFT
+            });
+        }
+
+        // Vérifie le voisin en bas
+        if (p.Y > 0 && !maze[p.X, p.Y - 1].HasFlag(WallState.VISITED))
+        {
+            neighbours.Add(new Neighbour
+            {
+                Position = new Position { X = p.X, Y = p.Y - 1 },
+                SharedWall = WallState.DOWN
+            });
+        }
+
+        // Vérifie le voisin en haut
+        if (p.Y < height - 1 && !maze[p.X, p.Y + 1].HasFlag(WallState.VISITED))
+        {
+            neighbours.Add(new Neighbour
+            {
+                Position = new Position { X = p.X, Y = p.Y + 1 },
+                SharedWall = WallState.UP
+            });
+        }
+
+        // Vérifie le voisin à droite
+        if (p.X < width - 1 && !maze[p.X + 1, p.Y].HasFlag(WallState.VISITED))
+        {
+            neighbours.Add(new Neighbour
+            {
+                Position = new Position { X = p.X + 1, Y = p.Y },
+                SharedWall = WallState.RIGHT
+            });
+        }
+
+        return neighbours;
+    }
+
+    /// <summary>
+    /// Génère un labyrinthe avec des murs initiaux partout
+    /// </summary>
+    /// <param name="width">Largeur de la grille</param>
+    /// <param name="height">Hauteur de la grille</param>
+    /// <returns>Labyrinthe généré</returns>
+    public static WallState[,] Generate(int width, int height)
+    {
+        var maze = new WallState[width, height];
+        WallState initialWallState = WallState.RIGHT | WallState.LEFT | WallState.UP | WallState.DOWN;
+
+        // Initialisation des murs
+        for (int x = 0; x < width; ++x)
+        {
+            for (int y = 0; y < height; ++y)
+            {
+                maze[x, y] = initialWallState;
+            }
+        }
+
+        return ApplyRecursiveBacktracker(maze, width, height);
+    }
+}
---
+++ Assets/Scripts/MazeRenderer.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Classe responsable du rendu du labyrinthe
+/// </summary>
+public class MazeRenderer : MonoBehaviour
+{
+    // Largeur du labyrinthe (paramètre modifiable dans l'inspecteur)
+    [SerializeField, Range(1, 50)] private int _width = 10;
+
+    // Hauteur du labyrinthe (paramètre modifiable dans l'inspecteur)
+    [SerializeField, Range(1, 50)] private int _height = 10;
+
+    // Taille des cellules et des murs
+    [SerializeField] private float _cellSize = 1f;
+
+    // Préfabriqué pour les murs
+    [SerializeField] private Transform _wallPrefab = null;
+
+    // Préfabriqué pour le sol
+    [SerializeField] private Transform _floorPrefab = null;
+
+    // Préfabriqué pour les pièces
+    [SerializeField] private Transform _coinPrefab = null;
+
+    // Nombre de pièces à placer dans le labyrinthe
+    [SerializeField, Range(1, 20)] private int _coinCount = 5;
+
+    /// <summary>
+    /// Méthode appelée au démarrage du script
+    /// </summary>
+    private void Start()
+    {
+        // Génération du labyrinthe
+        WallState[,] maze = MazeGenerator.Generate(_width, _height);
+
+        // Rendu du labyrinthe
+        RenderMaze(maze);
+
+        // Placement des pièces dans le labyrinthe
+        PlaceCoins(maze);
+    }
+
+    /// <summary>
+    /// Méthode responsable du rendu du labyrinthe
+    /// </summary>
+    /// <param name="maze">Matrice représentant l'état des murs du labyrinthe</param>
+    private void RenderMaze(WallState[,] maze)
+    {
+        // Instanciation du sol
+        RenderFloor();
+
+        // Parcours des cellules du labyrinthe pour dessiner les murs
+        for (int x = 0; x < _width; ++x)
+        {
+            for (int z = 0; z < _height; ++z)
+            {
+                WallState cell = maze[x, z];
+                Vector3 position = CalculateCellPosition(x, z);
+
+                // Dessin des murs si nécessaire
+                RenderWallIfExists(cell, WallState.UP, position + new Vector3(0, 0, _cellSize / 2));
+                RenderWallIfExists(cell, WallState.LEFT, position + new Vector3(-_cellSize / 2, 0, 0), 90);
+
+                // Dessin des murs de bordure (droite et bas)
+                if (x == _width - 1)
+                {
+                    RenderWallIfExists(cell, WallState.RIGHT, position + new Vector3(_cellSize / 2, 0, 0), 90);
+                }
+
+                if (z == 0)
+                {
+                    RenderWallIfExists(cell, WallState.DOWN, position + new Vector3(0, 0, -_cellSize / 2));
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Méthode pour instancier le sol du labyrinthe
+    /// </summary>
+    private void RenderFloor()
+    {
+        Transform floor = Instantiate(_floorPrefab, transform);
+        floor.localScale = new Vector3(_width, 1, _height);
+    }
+
+    /// <summary>
+    /// Méthode pour calculer la position d'une cellule
+    /// </summary>
+    /// <param name="x">Indice X de la cellule</param>
+    /// <param name="z">Indice Z de la cellule</param>
+    /// <returns>Position dans le monde réel de la cellule</returns>
+    private Vector3 CalculateCellPosition(int x, int z)
+    {
+        return new Vector3(-_width / 2 + x, 0, -_height / 2 + z);
+    }
+
+    /// <summary>
+    /// Méthode pour instancier un mur si le mur est requis
+    /// </summary>
+    /// <param name="cell">État des murs de la cellule</param>
+    /// <param name="wallState">Direction du mur à vérifier</param>
+    /// <param name="position">Position du mur</param>
+    /// <param name="rotationY">Rotation Y du mur (par défaut 0)</param>
+    private void RenderWallIfExists(WallState cell, WallState wallState, Vector3 position, float rotationY = 0)
+    {
+        if (cell.HasFlag(wallState))
+        {
+            Transform wall = Instantiate(_wallPrefab, transform);
+            wall.position = position;
+            wall.localScale = new Vector3(_cellSize, wall.localScale.y, wall.localScale.z);
+            wall.eulerAngles = new Vector3(0, rotationY, 0);
+        }
+    }
+
+    /// <summary>
+    /// Méthode pour placer des pièces à des positions aléatoires dans le labyrinthe
+    /// </summary>
+    /// <param name="maze">Matrice représentant l'état des murs du labyrinthe</param>
+    private void PlaceCoins(WallState[,] maze)
+    {
+        if (_coinPrefab == null)
+        {
+            Debug.LogError("Coin Prefab is not assigned in the Inspector!");
+            return;
+        }
+
+        var rng = new System.Random();
+        var placedPositions = new HashSet<Vector3>();
+
+        for (int i = 0; i < _coinCount; ++i)
+        {
+            int x, z;
+            Vector3 position;
+
+            // Trouver une position unique
+            do
+            {
+                x = rng.Next(0, _width);
+                z = rng.Next(0, _height);
+                position = CalculateCellPosition(x, z);
+            } while (placedPositions.Contains(position));
+
+            placedPositions.Add(position);
+
+            // Instancier la pièce
+            Transform coin = Instantiate(_coinPrefab, transform);
+            coin.position = position + new Vector3(0, 0.5f, 0); // Déplacer légèrement au-dessus du sol
+        }
+    }
+}
